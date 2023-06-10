0a4c39520c866db58c05b1ad887410a52b6cbc4a
ymj68520 <x68520@gmail.com>
2023-06-10 17:21:56 +0800

    Add http auto-redirect to https

---
+++ src/main/java/com/my/blog/website/config/ConnectorHttp2HttpsConfig.java
@@ -0,0 +1,41 @@
+package com.my.blog.website.config;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.naming.Context;
+
+/*
+@Configuration
+public class ConnectorHttp2HttpsConfig {
+    @Bean
+    TomcatEmbeddedServletContainerFactory tomcatEmbeddedServletContainerFactory(){
+        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory(){
+//            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint constraint = new SecurityConstraint();
+                constraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                constraint.addCollection(collection);
+                context.addConstraint(constraint);
+            }
+        };
+        factory.addAdditionalTomcatConnectors(createTomcatConnector());
+        return factory;
+    }
+
+    private Connector createTomcatConnector(){
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8081);
+        connector.setSecure(false);
+        connector.setRedirectPort(8080);
+        return connector;
+    }
+}
+*/
---
+++ src/main/java/com/my/blog/website/config/TomcatConfig.java
@@ -0,0 +1,45 @@
+package com.my.blog.website.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+/*
+ * Thanks for clmm123, this is blog: https://blog.csdn.net/baidu_37302589/article/details/100692957
+ * 重定向http请求到https
+ */
+@Configuration
+public class TomcatConfig {
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(createHttpConnector());
+        return tomcat;
+    }
+
+    private Connector createHttpConnector() {
+        Connector connector
+                = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setSecure(false);
+        connector.setPort(8081);
+        connector.setRedirectPort(8080);
+        return connector;
+    }
+
+}
